bab30b455be739dacc7a9700c783d14b4043b12a
Hengfei Zhuang <zhuanghengfei@gmail.com>
2014-02-25 22:08:59 +0800

    add test

--- src/webcache.js
+++ src/webcache.js
@@ -1,216 +1,188 @@
-;(function (name, definition) {
-	console.log("run");
-	var hasDefine = typeof define === 'function';
-	var hasExports = typeof module !== 'undefined' && module.exports;
+define("webcache", ["json2"], function () {
+	"use strict";
 
-	if (hasDefine) {
-		// AMD Module or CMD Module
-		define(definition);
-	} else if (hasExports) {
-		// Node.js Module
-		module.exports = function () {
-
-		};
-
-	} else {
-		// Assign to common namespaces or simply the global object (window)
-		this[name] = definition();
+	var _store = {
+		removeItem: function (k) {
+			delete this[k];
+		}
+	};
+	try {
+		if ("localStorage" in window && window["localStorage"]) {
+			var testStorage = window["localStorage"];
+			testStorage.setItem("test_storage", "");
+			testStorage.removeItem("test_storage");
+			_store = window["localStorage"];
+		}
+	} catch (e) {
 	}
 
-})('webcache', function () {
+	var isIE = "ActiveXObject" in window;
 
-	return function () {
-		"use strict";
+	function WebCache(name, size, timeout) {
+		var datas = {};
+		var frees = [];
+		var count = 0;
 
-		var _store = {
-			removeItem: function (k) {
-				delete this[k];
+		timeout = timeout || 0;
+
+		function load() {
+			function _new() {
+				datas = {};
+				frees = [];
+				count = size;
+				for (var i = 0; i < size; i++)
+					frees.push(i);
 			}
-		};
-		try {
-			if ("localStorage" in window && window["localStorage"]) {
-				var testStorage = window["localStorage"];
-				testStorage.setItem("test_storage", "");
-				testStorage.removeItem("test_storage");
-				_store = window["localStorage"];
+
+			try {
+				count = new Number(_store[name + '_count']);
+				if (count != size) {
+					for (var i = 0; i < count; i++) {
+						_store.removeItem(name + '_' + i);
+						_store.removeItem(name + '_' + i + '_time');
+					}
+					_store.removeItem(name + '_datas');
+					_store.removeItem(name + '_frees');
+					_store.removeItem(name + '_count');
+
+					_new();
+				} else {
+					datas = JSON.parse(_store[name + '_datas']);
+					frees = JSON.parse(_store[name + '_frees']);
+				}
+			} catch (e) {
+				_new();
 			}
-		} catch (e) {
 		}
 
-		var isIE = "ActiveXObject" in window;
+		function save() {
+			_store[name + '_datas'] = JSON.stringify(datas);
+			_store[name + '_frees'] = JSON.stringify(frees);
+			_store[name + '_count'] = count.toString();
+		}
 
-		function WebCache(name, size, timeout) {
-			var datas = {};
-			var frees = [];
-			var count = 0;
+		function _del(k) {
+			frees.push(datas[k]);
+			delete datas[k];
+			return false;
+		}
 
-			timeout = timeout || 0;
-
-			function load() {
-				function _new() {
-					datas = {};
-					frees = [];
-					count = size;
-					for (var i = 0; i < size; i++)
-						frees.push(i);
-				}
-
-				try {
-					count = new Number(_store[name + '_count']);
-					if (count != size) {
-						for (var i = 0; i < count; i++) {
-							_store.removeItem(name + '_' + i);
-							_store.removeItem(name + '_' + i + '_time');
-						}
-						_store.removeItem(name + '_datas');
-						_store.removeItem(name + '_frees');
-						_store.removeItem(name + '_count');
-
-						_new();
-					} else {
-						datas = JSON.parse(_store[name + '_datas']);
-						frees = JSON.parse(_store[name + '_frees']);
-					}
-				} catch (e) {
-					_new();
-				}
-			}
-
-			function save() {
-				_store[name + '_datas'] = JSON.stringify(datas);
-				_store[name + '_frees'] = JSON.stringify(frees);
-				_store[name + '_count'] = count.toString();
-			}
-
-			function _del(k) {
-				frees.push(datas[k]);
-				delete datas[k];
-				return false;
-			}
-
-			function _has(k) {
-				if (datas.hasOwnProperty(k)) {
-					if (timeout <= 0)
-						return true;
-
-					if (new Date().getTime() - new Number(_store[name + '_' + datas[k] + '_time']) >= timeout)
-						return _del(k);
+		function _has(k) {
+			if (datas.hasOwnProperty(k)) {
+				if (timeout <= 0)
 					return true;
-				} else
-					return false;
-			}
 
-			function _clean() {
-				if (timeout > 0) {
-					for (var k in datas)
-						_has(k);
-				}
-			}
+				if (new Date().getTime() - new Number(_store[name + '_' + datas[k] + '_time']) >= timeout)
+					return _del(k);
+				return true;
+			} else
+				return false;
+		}
 
-			function _put_data(n, v) {
-				_store[name + '_' + n] = JSON.stringify(v);
-				_store[name + '_' + n + '_time'] = new Date().getTime();
+		function _clean() {
+			if (timeout > 0) {
+				for (var k in datas)
+					_has(k);
+			}
+		}
+
+		function _put_data(n, v) {
+			_store[name + '_' + n] = JSON.stringify(v);
+			_store[name + '_' + n + '_time'] = new Date().getTime();
+			return v;
+		}
+
+		function _put(k, v) {
+			var n = frees.pop();
+			datas[k] = n;
+
+			return _put_data(n, v);
+		}
+
+		function access(k) {
+			var n = datas[k];
+
+			delete datas[k];
+
+			if (isIE) {
+				var datas1 = {};
+
+				for (var k1 in datas)
+					datas1[k1] = datas[k1];
+				datas = datas1;
+			}
+			datas[k] = n;
+
+			return n;
+		}
+
+		function _update(k, v) {
+			return _put_data(access(k), v);
+		}
+
+		// exists
+		this.has = function (k) {
+			load();
+
+			return _has(k);
+		}
+
+		// fetch data
+		this.get = function (k) {
+			load();
+
+			if (_has(k)) {
+				var v = JSON.parse(_store[name + '_' + access(k)]);
+				save();
 				return v;
 			}
-
-			function _put(k, v) {
-				var n = frees.pop();
-				datas[k] = n;
-
-				return _put_data(n, v);
-			}
-
-			function access(k) {
-				var n = datas[k];
-
-				delete datas[k];
-
-				if (isIE) {
-					var datas1 = {};
-
-					for (var k1 in datas)
-						datas1[k1] = datas[k1];
-					datas = datas1;
-				}
-				datas[k] = n;
-
-				return n;
-			}
-
-			function _update(k, v) {
-				return _put_data(access(k), v);
-			}
-
-			// exists
-			this.has = function (k) {
-				load();
-
-				return _has(k);
-			}
-
-			// fetch data
-			this.get = function (k) {
-				load();
-
-				if (_has(k)) {
-					var v = JSON.parse(_store[name + '_' + access(k)]);
-					save();
-					return v;
-				}
-			}
-
-			// update or insert
-			this.put = function (k, v) {
-				load();
-
-				if (_has(k))
-					_update(k, v);
-				else {
-					if (frees.length == 0) {
-						for (var k1 in datas) {
-							if (_has(k1)) {
-								if (frees.length == 0)
-									_del(k1);
-								break;
-							}
-						}
-					}
-
-					_put(k, v);
-				}
-
-				save();
-			}
-
-			// update if exists
-			this.set = function (k, v) {
-				load();
-				if (_has(k)) {
-					_update(k, v);
-					save();
-				}
-			}
-
-			this.datas = function () {
-				var ds = {};
-
-				load();
-				_clean();
-
-				for (var k in datas) {
-					ds[k] = JSON.parse(_store[name + '_' + datas[k]]);
-				}
-				return ds;
-			}
-
 		}
 
-		return WebCache;
-	};
+		// update or insert
+		this.put = function (k, v) {
+			load();
 
+			if (_has(k))
+				_update(k, v);
+			else {
+				if (frees.length == 0) {
+					for (var k1 in datas) {
+						if (_has(k1)) {
+							if (frees.length == 0)
+								_del(k1);
+							break;
+						}
+					}
+				}
+
+				_put(k, v);
+			}
+
+			save();
+		}
+
+		// update if exists
+		this.set = function (k, v) {
+			load();
+			if (_has(k)) {
+				_update(k, v);
+				save();
+			}
+		}
+
+		this.datas = function () {
+			var ds = {};
+
+			load();
+			_clean();
+
+			for (var k in datas) {
+				ds[k] = JSON.parse(_store[name + '_' + datas[k]]);
+			}
+			return ds;
+		}
+
+	}
+
+	return WebCache;
 });
-
-
-//define("webcache", function() {
-//
-//	});
